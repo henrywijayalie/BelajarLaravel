784c048b16a4cec96f2f18872ac57cc586c08730
Henry Wijaya <henrywijaya92@gmail.com>
2021-06-14 16:28:27 +0700

    commit

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/about', function () {
+    return view('about');
+});
+
+
+Route::get('/', function () {
+    return view('layout/main');
+});
+
+
 Route::get('/', function () {
     return view('layout/main');
 });
